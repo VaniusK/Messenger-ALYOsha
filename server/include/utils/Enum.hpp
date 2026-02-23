8f85ef5eae2d834dcb9da7e2705e631dbdf8cfcf
konobeitsev3 <konobeitsev3@gmail.com>
2026-02-23 14:02:28 +0300

    feat: Enums for SQL

---
+++ server/include/utils/Enum.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+namespace messenger::models {
+
+namespace ChatRole {
+inline const std::string Member = "member";
+inline const std::string Moderator = "moderator";
+inline const std::string Admin = "admin";
+inline const std::string Owner = "owner";
+}  // namespace ChatRole
+
+namespace ChannelRole {
+inline const std::string Member = "member";
+inline const std::string Admin = "admin";
+inline const std::string Owner = "owner";
+}  // namespace ChannelRole
+
+namespace ChatType {
+inline const std::string Direct = "direct";
+inline const std::string Group = "group";
+inline const std::string Channel = "discussion";
+inline const std::string Saved = "saved";
+}  // namespace ChatType
+
+}  // namespace messenger::models
