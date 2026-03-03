73bbf2a421eeb051da4d53a6b412d1eb694d6f89
dast3rr <vovdimon@yandex.ru>
2026-03-03 15:37:57 +0300

    feat: add one-ip-ddos-attack protection

---
+++ server/filters/api_v1_IpFilter.cc
@@ -0,0 +1,70 @@
+/**
+ *
+ *  api_v1_IpFilter.cc
+ *
+ */
+
+#include "api_v1_IpFilter.h"
+#include <chrono>
+#include <mutex>
+#include <shared_mutex>
+#include <drogon/HttpAppFramework.h>
+#include <json/value.h>
+
+using namespace drogon;
+using namespace api::v1;
+
+std::unordered_map<std::string, ClientRequestsCounter> IpFilter::clients_;
+std::shared_mutex IpFilter::mutex_;
+
+IpFilter::IpFilter(){
+    drogon::app().getLoop()->runEvery(std::stoi(std::getenv("SERVER_IP_LIST_CLEANING_SECONDS_COOLDOWN")), [this](){
+        this->cleanUpOldClients();
+    });
+}
+
+void IpFilter::cleanUpOldClients(){
+    auto now = std::chrono::steady_clock::now();
+
+    std::unique_lock<std::shared_mutex> lock(mutex_);
+
+    std::size_t size_before = clients_.size();
+    std::erase_if(clients_, [now, this](const auto& item){
+        auto [ip, client] = item;
+        auto time_diff = std::chrono::duration_cast<std::chrono::seconds>(now - client.window_start);
+
+        return time_diff > std::chrono::seconds(WINDOW_SECONDS);
+    });
+    LOG_INFO << "Cleaned " << (size_before - clients_.size()) << " old ips.";
+
+}
+
+void IpFilter::doFilter(const HttpRequestPtr &req,
+                         FilterCallback &&fcb,
+                         FilterChainCallback &&fccb)
+{
+    std::string ip = req->getPeerAddr().toIp();
+    auto now = std::chrono::steady_clock::now();
+    {
+        std::unique_lock<std::shared_mutex> lock(mutex_);
+        auto &client = clients_[ip];
+        auto time_diff = std::chrono::duration_cast<std::chrono::seconds>(now - client.window_start);
+        if (time_diff > std::chrono::seconds(WINDOW_SECONDS)){
+            client.requests_count = 1;
+            client.window_start = now;
+        }
+        else{
+            client.requests_count++;
+            if (client.requests_count > MAX_REQUESTS){
+                Json::Value response_json;
+                response_json["message"] = "Too many requests";
+                LOG_WARN << "Too many requests from " << ip;
+                auto response = HttpResponse::newHttpJsonResponse(response_json);
+                response->setStatusCode(k429TooManyRequests);
+                fcb(response);
+                return;
+            } 
+        }
+        fccb();
+    }
+}
---
+++ server/filters/api_v1_IpFilter.h
@@ -0,0 +1,43 @@
+/**
+ *
+ *  api_v1_IpFilter.h
+ *
+ */
+
+#pragma once
+
+#include <drogon/HttpFilter.h>
+#include <unordered_map>
+#include <chrono>
+#include <shared_mutex>
+using namespace drogon;
+namespace api
+{
+namespace v1
+{
+
+struct ClientRequestsCounter{
+  int requests_count = 0;
+  std::chrono::steady_clock::time_point window_start;
+};
+
+class IpFilter : public HttpFilter<IpFilter>
+{
+  public:
+    IpFilter();
+    void doFilter(const HttpRequestPtr &req,
+                  FilterCallback &&fcb,
+                  FilterChainCallback &&fccb) override;
+
+  private:
+    static std::unordered_map<std::string, ClientRequestsCounter> clients_;
+    static std::shared_mutex mutex_;
+
+    void cleanUpOldClients();
+
+    const int MAX_REQUESTS = std::stoi(std::getenv("SERVER_MAX_REQUESTS_PER_WINDOW"));
+    const int WINDOW_SECONDS = std::stoi(std::getenv("SERVER_WINDOW_FOR_REQUESTS_SECONDS"));
+};
+
+}
+}
