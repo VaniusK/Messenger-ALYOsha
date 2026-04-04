8b78e231826dd6570c8e934d7a6e4efeab12442c
Peter Antropovich <chelovek190807@yandex.ru>
2026-04-05 01:26:29 +0300

    feat: Persistent session with "Remember me" checkbox

--- client/src/core/StateManager.cpp
+++ client/src/core/StateManager.cpp
@@ -36,6 +36,9 @@ void StateManager::setUserId(int id) {
     if (m_userId != id) {
         m_userId = id;
         emit userIdChanged();
+        if (isLoggedIn() && m_rememberMe) {
+            saveSession();
+        }
     }
 }
 
@@ -43,8 +46,35 @@ void StateManager::clearState() {
     setToken("");
     setCurrentUserHandle("");
     setUserId(-1);
+    saveSession();
 }
 
 bool StateManager::isLoggedIn() const {
     return !m_token.isEmpty();
+}
+
+void StateManager::saveSession() {
+    QSettings settings("MessangerAlyosha", "Session");
+    settings.setValue("token", m_token);
+    settings.setValue("userId", m_userId);
+    settings.setValue("handle", m_currentUserHandle);
+}
+
+void StateManager::loadSession() {
+    QSettings settings("MessangerAlyosha", "Session");
+    m_token = settings.value("token", "").toString();
+    m_userId = settings.value("userId", -1).toInt();
+    m_currentUserHandle = settings.value("handle", "").toString();
+
+    emit tokenChanged();
+    emit userIdChanged();
+    emit currentUserHandleChanged();
+}
+
+bool StateManager::getRememberMe() const {
+    return m_rememberMe;
+}
+
+void StateManager::setRememberMe(bool rememberMe) {
+    m_rememberMe = rememberMe;
 }
